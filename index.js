c731cefcc0fb9069400cd6cedec02d0ff09c9e38
krisvu1 <vuvietanh677@gmail.com>
2022-06-11 22:43:50 +0700

    firt commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var obj = document.getElementById("hoverme");
+var obj2 = document.getElementById("click");
+obj2.onmouseover = function(){
+    document.getElementById("click").style.display = "block";
+}
+obj2.onmouseout = function(){
+    document.getElementById("click").style.display = "none";
+}
+obj.onmouseover = function(){
+    document.getElementById("click").style.display = "block";
+}
+obj.onmouseout = function(){
+    document.getElementById("click").style.display = "none";
+}
+
+
+
+
+
+var objmenu = document.getElementById("hoverthucdon")
+var objmenu2 = document.getElementById("click2")
+objmenu2.onmouseover = function(){
+    document.getElementById("click2").style.display = "block";
+}
+objmenu2.onmouseout = function(){
+    document.getElementById("click2").style.display = "none";
+}
+objmenu.onmouseover = function(){
+    document.getElementById("click2").style.display = "block";
+}
+objmenu.onmouseout = function(){
+    document.getElementById("click2").style.display = "none";
+}
